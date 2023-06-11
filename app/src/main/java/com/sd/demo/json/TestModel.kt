333a6e35c40cd68080d15398ae38fb46aebeef14
zj565061763 <565061763@qq.com>
2023-06-11 12:50:39 +0800

    demo

--- app/src/main/java/com/sd/demo/json/TestModel.kt
+++
@@ -1,9 +0,0 @@
-package com.sd.demo.json
-
-data class TestModel(
-    val valueInt: Int,
-    val valueLong: Long,
-    val valueFloat: Float,
-    val valueDouble: Double,
-    val valueBoolean: Boolean,
-)
