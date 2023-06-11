11b8afba4c859ce6ba7f376781b19b9c1ad8bf36
zj565061763 <565061763@qq.com>
2023-06-11 12:29:38 +0800

    test

--- app/src/androidTest/java/com/sd/demo/json/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sd/demo/json/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package com.sd.demo.json
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.gson.GsonBuilder
+import com.sd.lib.json.FJson
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,19 +14,15 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    fun test() {
-        val json = """
-            {"valueInt":"1","valueLong":"2","valueFloat":"3.3","valueDouble":"4.4","valueBoolean":"true"}
-        """.trimIndent()
-
-        val gson = GsonBuilder().create()
-
-        gson.fromJson(json, TestModel::class.java).let { model ->
-            assertEquals(1, model.valueInt)
-            assertEquals(2, model.valueLong)
-            assertEquals(3.3f, model.valueFloat)
-            assertEquals(4.4, model.valueDouble, 0.01)
-            assertEquals(true, model.valueBoolean)
+    fun testInt() {
+        FJson.jsonToObject(
+            """
+            {"value1":"1","value2":"2"}
+        """.trimIndent(),
+            IntModel::class.java,
+        ).let { model ->
+            assertEquals(1, model.value1)
+            assertEquals(2, model.value2)
         }
     }
 }
