373d2e3c342577c1f905925dd026c44ebe04d612
zj565061763 <565061763@qq.com>
2023-08-30 14:04:10 +0800

    1.3.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "json"
-val libVersionName = "1.2.0"
+val libVersionName = "1.3.0"
 
 android {
     namespace = "com.sd.lib.json"
