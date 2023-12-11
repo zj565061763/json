397d1e79cde0426e692f7d2634f96972c6b1df01
zjun <565061763@qq.com>
2023-12-11 14:25:42 +0800

    demo

--- app/src/main/java/com/sd/demo/json/model/RequestParams.kt
+++ app/src/main/java/com/sd/demo/json/model/RequestParams.kt
@@ -1,44 +1,28 @@
 package com.sd.demo.json.model
 
-import com.squareup.moshi.Json
-
-/**
- * Http请求参数
- */
-@PublishedApi
-internal open class RequestParams {
-    @Json(name = "data")
-    private var _data: MutableMap<String, String>? = null
+open class RequestParams {
+    protected var data: MutableMap<String, String>? = null
 
     fun put(key: String, value: String) {
-        val map = _data ?: mutableMapOf<String, String>().also {
-            _data = it
+        val map = data ?: mutableMapOf<String, String>().also {
+            data = it
         }
         map[key] = value
     }
 }
 
-/**
- * 带分页的Http请求参数
- */
-@PublishedApi
-internal class PageRequestParams : RequestParams() {
-    @Json(name = "pageQueryReq")
-    private var _page: RequestPage? = null
+class PageRequestParams : RequestParams() {
+    private var page: RequestPage? = null
 
     fun setPage(page: Int) {
-        _page = RequestPage(
+        this.page = RequestPage(
             page = page,
             pageSize = 10,
         )
     }
 }
 
-/**
- * 分页数据
- */
-internal data class RequestPage(
-    @Json(name = "page") val page: Int = 1,
-
-    @Json(name = "pageSize") val pageSize: Int = 10,
+data class RequestPage(
+    val page: Int = 1,
+    val pageSize: Int = 10,
 )
