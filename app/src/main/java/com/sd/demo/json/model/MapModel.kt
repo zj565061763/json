56e0a18e32f3d86541b62c3e4f27bb2294bb7b34
zjun <565061763@qq.com>
2023-12-11 13:40:23 +0800

    demo

--- app/src/main/java/com/sd/demo/json/model/MapModel.kt
+++ app/src/main/java/com/sd/demo/json/model/MapModel.kt
@@ -2,4 +2,8 @@ package com.sd.demo.json.model
 
 class MapModel {
     var name = "default"
+
+    var map: MutableMap<String, String> = mutableMapOf<String, String>().apply {
+        put("key", "value")
+    }
 }
